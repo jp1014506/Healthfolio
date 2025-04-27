c7f8d4f90c11f416a0ed9f3f96677d8b7c8740a7
NS977255 <112407729+NS977255@users.noreply.github.com>
2025-04-27 08:57:15 -0400

    Color expands with website

--- healthfolio/src/App.js
+++ healthfolio/src/App.js
@@ -3,7 +3,7 @@ import TheBigBabe from './components/TheBigBabe';
 
 function App() {
   return (
-    <div className="App h-dvh" style = {{ backgroundColor: "#B6E0CA"}}>
+    <div className="App min-h-screen" style = {{ backgroundColor: "#B6E0CA"}}>
       <TheBigBabe />
     </div>
   );
